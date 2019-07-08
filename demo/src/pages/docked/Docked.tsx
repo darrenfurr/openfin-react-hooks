bc2690984490d8670af8a4b9242ae82e9ebc7c12
Darren Furr <dfurr@tradewestsystems.com>
2019-07-08 16:50:47 -0500

    testing notifications

--- demo/src/pages/docked/Docked.tsx
+++ demo/src/pages/docked/Docked.tsx
@@ -1,9 +1,8 @@
-import {useDocked} from "openfin-react-hooks";
-import {_Window} from "openfin/_v2/api/window/window";
+import { useDocked } from "openfin-react-hooks";
+import { _Window } from "openfin/_v2/api/window/window";
 import * as Prism from "prismjs";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import uuidv4 from "uuid/v4";
-
 import styles from "./Docked.module.css";
 
 const codeExample = `import {useMaximized} from "openfin-react-hooks";
@@ -23,9 +22,13 @@ const Component = () => {
 const Docked: React.FC = () => {
     const [isDocked, undock] = useDocked();
 
-    useEffect(Prism.highlightAll, []);
+    const showNotification = () => {
+        new window.fin.desktop.Notification({ url: "/dock-window.html", message: { securityId: "123456789b", messageText: "moved to the top" }, onMessage: (message: any) => console.log('you got a message!', message) });
+    }
 
     useEffect(() => {
+        Prism.highlightAll();
+
         let win: _Window;
         const createWindow = async () => {
             win = await window.fin.Window.create({
@@ -44,13 +47,14 @@ const Docked: React.FC = () => {
                 win.close();
             }
         };
+
     }, []);
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>useDocked</h1>
             <div className={styles.description}>
-              Try docking / undocking the window to the opened child window to demonstrate this hook
+                Try docking / undocking the window to the opened child window to demonstrate this hook
             </div>
             <h2>Code Example</h2>
             <pre>
@@ -61,6 +65,7 @@ const Docked: React.FC = () => {
             <h2>Try it out</h2>
             <div>Window is <strong>{isDocked ? "docked" : "not docked"}</strong></div>
             <button type="button" disabled={!isDocked} onClick={undock}>Undock Window</button>
+            <button type="button" onClick={() => showNotification()}>Show Notification</button>
         </div>
     );
 };
